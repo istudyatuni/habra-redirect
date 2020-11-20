734b30a69361a456e53c163fc1710438702e9a80
Ilia <fortyhell@gmail.com>
2020-11-20 19:18:57 +0300

    Fix link
    Like https://habr.com/ru/company/cloud4y/news/t/520484/

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
 let habra = 'https://habra.js.org/post/'
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
+		// /332412/
+		let postRegexp = /\/\d+\//g;
+		// only digits
 		let numRegexp = /\d+/g;
-		return {redirectUrl: habra + details.url.match(numRegexp)[0]};
+
+		let post = details.url.match(postRegexp)[0];
+		post = post.match(numRegexp)[0];
+		return {redirectUrl: habra + post};
 	},
 	{
 		urls: ["*://*.habr.com/*"],
