fd32577da1823b3f52706c06e47e6917a59f8468
Ilia <fortyhell@gmail.com>
2020-11-21 06:52:59 +0300

    Delete unnecessary

--- background.js
+++ background.js
@@ -1,14 +1,9 @@
-let habra = 'https://habra.js.org/post/'
+let habra = 'https://habra.js.org/post'
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
-		// /332412/
+		// /xxxxxx/
 		let postRegexp = /\/\d+\//g;
-		// only digits
-		let numRegexp = /\d+/g;
-
-		let post = details.url.match(postRegexp)[0];
-		post = post.match(numRegexp)[0];
-		return {redirectUrl: habra + post};
+		return {redirectUrl: habra + details.url.match(postRegexp)[0]};
 	},
 	{
 		urls: ["*://*.habr.com/*"],
